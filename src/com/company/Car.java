1e3065b1889f1a2658983739cdc5d5676f2b6359
Jeremy <jeremyeparker@yahoo.com>
2021-07-12 20:42:04 -0400

    whip

---
+++ src/com/company/Car.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car extends Vehicle{
+    List<Tire> tires = new ArrayList<>();
+
+//    public Car( Engine engine,
+//                String type,
+//                boolean isAir,
+//                boolean isLand,
+//                boolean isSpace,
+//                boolean isWater){
+//        super(engine,type,isAir,isLand,isSpace,isWater);
+//        this.type = "Car";
+//        this.isAir = false;
+//        this.isLand = true;
+//        this.isSpace = false;
+//        this.isWater = false;
+//    }
+    public Car(Engine engine){
+        super(engine,"Car",false,true,false,false,4);
+
+    }
+
+    public void addTire(Tire tire){
+        tires.add(tire);
+//        System.out.print(tires.size() + " total tires");
+
+    }
+    public void removeTire(){
+        tires.remove(0);
+        System.out.println("remove tire");
+    }
+    public void replaceTire(int tireIndex, Tire replacement){
+        tires.remove(tireIndex);
+        tires.add(replacement);
+        System.out.println("tire replaced");
+    }
+}
