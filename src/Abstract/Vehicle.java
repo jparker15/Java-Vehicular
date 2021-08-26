f5c3da90c45544c35658385e199ff73c30f127ca
Jeremy <jeremyeparker@yahoo.com>
2021-08-26 18:15:19 -0400

    child classs

--- src/Abstract/Vehicle.java
+++ src/Abstract/Vehicle.java
@@ -1,2 +1,32 @@
-package Abstract;public class Vehicle {
+package Abstract;
+
+public class Vehicle extends Engine{
+
+
+    int year;
+    String make;
+    String model;
+    String color;
+    boolean isRunning = false;
+
+
+    public void startEngine(){
+        if(!isRunning){
+            isRunning = true;
+            System.out.println("Engine is running");
+        }else{
+            System.out.println("Engine already running");
+        }
+    }
+
+    public void stopEngine(){
+        if(isRunning){
+            isRunning = false;
+            System.out.println("Engine off");
+        }else{
+            System.out.println("Engine already off");
+        }
+    }
+
+
 }
