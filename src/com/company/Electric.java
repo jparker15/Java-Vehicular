15dbcadcff9fad6ec4656bcb953652211a227201
Jeremy <jeremyeparker@yahoo.com>
2021-08-30 19:21:59 -0400

    jkasd

--- src/com/company/Electric.java
+++ src/com/company/Electric.java
@@ -1,2 +1,16 @@
-package com.company;public class Electric {
+package com.company;
+
+public class Electric extends Engine{
+
+    String type = "Electric";
+    @Override
+    public void start(){
+        isRunning = true;
+        System.out.println("Electric is running: " + isRunning);
+    }
+
+    public void stop(){
+        isRunning = false;
+        System.out.println("Electric is running: " + isRunning);
+    }
 }
