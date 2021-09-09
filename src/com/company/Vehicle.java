41994f813d1768f7a88bff1c195f6571088bb7be
Jeremy <jeremyeparker@yahoo.com>
2021-09-09 18:23:58 -0400

    entire class

--- src/com/company/Vehicle.java
+++ src/com/company/Vehicle.java
@@ -2,11 +2,19 @@ package com.company;
 
 public class Vehicle  {
 
-    int year;
-    String make;
-    String model;
-    String color;
-    Engine engine;
+   private int year;
+   private String make;
+   private String model;
+   private String color;
+   private Engine engine;
+   private int accelerationRate = 10;
+   private int coastRate = 3;
+   private int brakeRate = 5;
+   private int MPH = 0;
+
+   public Vehicle(){
+
+   }
 
     public Vehicle(int year, String make, String model, String color, Engine engine){
         this.year = year;
@@ -22,6 +30,37 @@ public class Vehicle  {
     public void stop(){
         engine.stop();
     }
+
+    public int getMPH(){
+       return MPH;
+    }
+
+    public void accelerate(){
+       if(MPH >= 125) {
+           System.out.println("\nREDLINE!\n");
+       }else{
+           System.out.println("\nGAS GAS GAS\n");
+           MPH += accelerationRate;
+       }
+    };
+
+    public void coast(){
+        if(MPH > 0){
+            System.out.println("\nCoast\n");
+            MPH -= coastRate;
+        }else{
+            System.out.println("\nVehicle is stopped\n");
+        }
+    };
+
+    public void decelerate(){
+        if(MPH > 0){
+            System.out.println("\nBrake\n");
+            MPH -= coastRate;
+        }else{
+            System.out.println("\nVehicle is stopped\n");
+        }
+    };
 
     @Override
     public String toString(){
